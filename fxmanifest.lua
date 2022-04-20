6400bd8733c8834d39f220328a85d337b68e908b
Eyesmack <40875081+Eyesmack@users.noreply.github.com>
2022-04-20 19:12:19 +1200

    Changed version number
    Changed version number to best represent what state the mod is in. And slightly changed the description.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Isaac Mason'
-description 'Simple Vehicle Spawner'
-version '1.1.3'
+description 'A simple Vehicle Spawner'
+version '0.1.3'
 
 client_script 'client.lua'
